ce2b973fc7cd5629358b220250fe48888922346f
Nax <max@bacoux.com>
2017-12-25 19:58:23 +0100

    Clean the sema_builder more

--- lib/magma/sema_builder.rb
+++ lib/magma/sema_builder.rb
@@ -48,13 +48,14 @@ module Magma
       ast_fun.params.each_with_index do |param, i|
         var_table[param.name] = i
       end
+      @var_table_stack = [var_table]
       ast_fun.block.statements.each do |stmt|
-        visit(stmt, var_table)
+        visit(stmt)
       end
     end
 
-    def statement_cond(stmt, var_table)
-      e = visit(stmt.expr, var_table)
+    def statement_cond(stmt)
+      e = visit(stmt.expr)
       block_true = @sema_fun.add_basic_block
       if stmt.block_false
         block_false = @sema_fun.add_basic_block
@@ -65,14 +66,14 @@ module Magma
       @block_stack << block_next
       @block_stack << block_true
       stmt.block_true.statements.each do |s|
-        visit(s, var_table)
+        visit(s)
       end
       block_true_end = @block_stack.pop
       block_true_end.add_jump(block_next)
       if block_false
         @block_stack << block_false
         stmt.block_false.statements.each do |s|
-          visit(s, var_table)
+          visit(s)
         end
         block_false_end = @block_stack.pop
         block_false_end.add_jump(block_next)
@@ -82,76 +83,76 @@ module Magma
       end
     end
 
-    def statement_return(stmt, var_table)
+    def statement_return(stmt)
       unless stmt.expr.nil?
-        expr = visit(stmt.expr, var_table)
+        expr = visit(stmt.expr)
       end
       @block_stack.last.add_return(expr)
     end
 
-    def statement_expr(stmt, var_table)
-      @block_stack.last.add_expr(visit(stmt.expr, var_table))
+    def statement_expr(stmt)
+      @block_stack.last.add_expr(visit(stmt.expr))
     end
 
-    def statement_variable(stmt, var_table)
+    def statement_variable(stmt)
       name = stmt.name
       type = @sema.types[stmt.type]
       id = @sema_fun.add_var(type)
-      var_table[name] = id
+      @var_table_stack.last[name] = id
     end
 
-    def statement_while(stmt, var_table)
+    def statement_while(stmt)
       cond_block = @sema_fun.add_basic_block
       loop_block = @sema_fun.add_basic_block
       next_block = @sema_fun.add_basic_block
       current_block = @block_stack.pop
-      e = visit(stmt.expr, var_table)
+      e = visit(stmt.expr)
       cond_block.add_cond(e, loop_block, next_block)
       current_block.add_jump(cond_block)
       @block_stack << next_block
       @block_stack << loop_block
-      stmt.block.statements.each {|s| visit(s, var_table)}
+      stmt.block.statements.each {|s| visit(s)}
       loop_block_end = @block_stack.pop
       loop_block_end.add_jump(cond_block)
     end
 
-    def expr_assign(expr, var_table)
+    def expr_assign(expr)
       name = expr.name
-      id = var_table[name]
+      id = @var_table_stack.last[name]
       type = @sema_fun.vars[id]
-      e = visit(expr.expr, var_table)
+      e = visit(expr.expr)
       Sema::Expr.assign(id, type, e)
     end
 
-    def expr_identifier(expr, var_table)
+    def expr_identifier(expr)
       name = expr.name
-      id = var_table[name]
+      id = @var_table_stack.last[name]
       type = @sema_fun.vars[id]
       Sema::Expr.variable(id, type)
     end
 
-    def expr_literal(expr, var_table)
+    def expr_literal(expr)
       type = expr.type
       value = expr.value
       sema_type = @sema.types[type]
       Sema::Expr.literal(sema_type, value)
     end
 
-    def expr_binary(expr, var_table)
-      lhs = visit(expr.lhs, var_table)
-      rhs = visit(expr.rhs, var_table)
+    def expr_binary(expr)
+      lhs = visit(expr.lhs)
+      rhs = visit(expr.rhs)
       Sema::Expr.binary(expr.op, lhs, rhs)
     end
 
-    def expr_unary(expr, var_table)
-      e = visit(expr.expr, var_table)
+    def expr_unary(expr)
+      e = visit(expr.expr)
       Sema::Expr.unary(expr.op, e)
     end
 
-    def expr_call(expr, var_table)
+    def expr_call(expr)
       fun_name = expr.name
       args = expr.arguments.map { |a|
-        visit(a, var_table)
+        visit(a)
       }
       decl = @sema.decls_with_name(fun_name).first
       Sema::Expr.call(decl, args)
